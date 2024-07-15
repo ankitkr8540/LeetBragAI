139a1e43fb4cd69a09f8517455b660ac42d82016
lingyundai <lingyundaiseven@gmail.com>
2024-07-15 19:34:37 -0400

    remove commented out code

--- frontend/src/screens/Login.js
+++ frontend/src/screens/Login.js
@@ -80,38 +80,6 @@ const LoginScreen = ({ onClose }) => {
     </div>,
     document.body
   );
-  // <FormContainer>
-  //   <h1>Sign In</h1>
-  //   {loading && <h1>Loading...</h1>}
-  //   <Form onSubmit={submitHandler}>
-  //     <Form.Group controlId="username">
-  //       <Form.Label>Enter Username</Form.Label>
-  //       <Form.Control
-  //         type="text"
-  //         placeholder="Enter Leetcode Username"
-  //         value={username}
-  //         onChange={(e) => setUsername(e.target.value)}
-  //       ></Form.Control>
-  //     </Form.Group>
-
-  //     <Form.Group controlId="password">
-  //       <Form.Label>Enter Password</Form.Label>
-  //       <Form.Control
-  //         type="password"
-  //         placeholder="Enter Password"
-  //         value={password}
-  //         onChange={(e) => setPassword(e.target.value)}
-  //       ></Form.Control>
-  //     </Form.Group>
-  //     <Row className="py-2">
-  //       <Col>
-  //         <Button type="submit" variant="primary">
-  //           Sign In
-  //         </Button>
-  //       </Col>
-  //     </Row>
-  //   </Form>
-  // </FormContainer>
 };
 
 export default LoginScreen;
